57a0cedffbf80febbbcbd83cefab0eca43a65a50
wykys <vvykys@gmail.com>
2021-08-16 17:54:30 +0200

    Added basic user app for testing

---
+++ app/src/main.c
@@ -0,0 +1,19 @@
+#include "stm8s.h"
+
+#define _ISOC99_SOURCE
+#define _GNU_SOURCE
+
+void main(void)
+{
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // FREQ MCU 16MHz
+    GPIO_Init(GPIOC, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_SLOW);
+
+    while (1)
+    {
+        GPIO_WriteReverse(GPIOC, GPIO_PIN_5);
+        for (uint32_t i = 0; i < 100000; i++)
+            ;
+    }
+}
+
+#include "__assert__.h"
