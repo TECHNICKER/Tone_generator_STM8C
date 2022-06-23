a871372433f7cfd82510407c8ac7cc1370cc72ec
David Logr <logrik@email.cz>
2022-06-23 17:48:12 +0200

    začátek práce

--- app/src/main.c
+++ app/src/main.c
@@ -3,12 +3,16 @@
 void main(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // FREQ MCU 16MHz
-    GPIO_Init(GPIOC, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(GPIOB, GPIO_PIN_0, GPIO_MODE_IN_PU_NO_IT);
+    GPIO_Init(GPIOB, GPIO_PIN_1, GPIO_MODE_IN_PU_NO_IT);
+    GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_OUT_PP_LOW_SLOW);
 
     while (1)
     {
-        GPIO_WriteReverse(GPIOC, GPIO_PIN_5);
-        for (uint32_t i = 0; i < 100000; i++)
-            ;
+        if (GPIO_ReadInputPin(GPIOB, GPIO_PIN_1) == RESET)
+        {
+            GPIO_WriteReverse(GPIOD, GPIO_PIN_6);
+            for (uint32_t i = 0; i < 1000; i++);
+        }
     }
 }
